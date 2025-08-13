1d427c2225ee7519196d2009372e17d65aaea077
Nikolai <nikolai.ahlhelm@gmail.com>
2025-08-13 16:18:45 +0200

    Add Config and ConfigManager classes for configuration handling

---
+++ CSLM/CSLM.Config/Config.cs
@@ -0,0 +1,10 @@
+namespace CSLM.Config;
+
+public class Config
+{
+     string logFileName;
+    private string logFilePath;
+    private string logType = "DEFAULT";
+    private bool printToConsole = true;
+    private string timestampFormat = "dd-MM-yyyy HH:mm:ss.fff";
+}
---
+++ CSLM/CSLM.Config/ConfigManager.cs
@@ -0,0 +1,6 @@
+namespace CSLM.Config;
+
+public class ConfigManager
+{
+    
+}

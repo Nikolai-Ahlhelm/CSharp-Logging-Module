8fd8602ca811348435f7a5c3dd2c88a96a2dba5f
Nikolai <nikolai.ahlhelm@gmail.com>
2025-08-14 15:25:50 +0200

    Refactor Config class and add storage configs
    Reorganized the Config class to use internal fields and added separate configuration classes for File, SQL, and SQLite log storage options. This improves modularity and prepares for supporting multiple log storage backends.

--- CSLM/CSLM.Config/Config.cs
+++ CSLM/CSLM.Config/Config.cs
@@ -2,9 +2,34 @@ namespace CSLM.Config;
 
 public class Config
 {
-     string logFileName;
-    private string logFilePath;
-    private string logType = "DEFAULT";
-    private bool printToConsole = true;
-    private string timestampFormat = "dd-MM-yyyy HH:mm:ss.fff";
+    internal string logType = "DEFAULT";
+    internal bool printToConsole = true;
+    internal string timestampFormat = "dd-MM-yyyy HH:mm:ss.fff";
+    internal string logStoreType = "File"; // Options: SQL, SQLite, File
+    internal FileConfig fileConfig = new FileConfig();
+    internal SQLiteConfig sqliteConfig = new SQLiteConfig();
+    internal SQLConfig sqlConfig = new SQLConfig();
+}
+
+internal class FileConfig
+{
+    private string filePath = "\\logs";
+    private string fileName = "log-%hh%-%m%-%ss%.txt";
+}
+
+internal class SQLConfig
+{
+    
+    private string sqlServer = "localhost";
+    private int sqlPort = 1433;
+    private string sqlUser = "cslm_user";
+    private string sqlPassword = "password";
+    private string databaseName = "CSLM";
+    private string tablePrefix = "CSLM_";
+}
+
+internal class SQLiteConfig
+{
+    private string sqliteFilePath = "cslm.db";
+    private string tablePrefix = "CSLM_";
 }
